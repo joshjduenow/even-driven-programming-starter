47c245e3346261b017e5fba42bf72a8c3e0b2656
Joshua Duenow <joshjduenow@gmail.com>
2023-10-26 12:54:29 -0500

    potato

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function onReady() {
   console.log('Yay!, onReady() got called when the page loaded.');
+  // We can select things!!
+  // document is the HTML file...
+  // querySelector will match 1 thing. The first thing
+  let header = document.querySelector('h1')
+  // document.querySelector('.class')
+  // document.getElementById('container')
+  console.log(header);
+  // manipulating
+  header.classList.add('pink-text')
 
+  header.textContent = 'Hello from JavaScript!'
 }
 
+function onPotatoClick() {
+  console.log('You clicked potato!');
+
+  let potatoFarm = document.getElementById('potatoFarm');
+
+  potatoFarm.innerHTML += '<p>🥔</p>'
+}
 // Will call onReady() when the page loads.
 onReady()
